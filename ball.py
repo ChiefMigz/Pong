d49b8a94d6161878c37ef761efd9adc7980c4d60
ChiefMigz <pebenitomiguel@gmail.com>
2022-04-28 02:06:17 -0700

    Updated paddle.py

--- ball.py
+++ ball.py
@@ -7,11 +7,9 @@ class Ball(pygame.sprite.Sprite):
         
         # Pass in the color of the paddle, its width and height.
         # Set the background color and set it to be transparent
-        self.image = pygame.Surface([width + radius, height + radius])
+        self.image = pygame.Surface([width, height])
         self.image.fill("BLACK")
         self.image.set_colorkey("BLACK")
-        self.xVelocity = 1
-        self.yVelocity = 1
 
         # Draw the paddle (a rectangle!)
         pygame.draw.circle(self.image, color, [width, height], radius)
